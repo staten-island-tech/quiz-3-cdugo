bec79e93cb29ea09bd6d5b8bbf1cba48441e84c8
cdugo <54993517+cdugo@users.noreply.github.com>
2019-09-16 11:51:07 -0400

    quiz 3

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+function tipCalculator(bills) {
+    if (bills < 50 ) {
+        percentage = .2;
+    }
+    else if (bills >= 50 && bills <= 200){
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * bills;
+
+}
+
+const bills = [140, 45, 270];
+
+const tips = [tipCalculator(bills[0]), tipCalculator(bills[1]), tipCalculator(bills[2])];
+
+const finalCost = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+
